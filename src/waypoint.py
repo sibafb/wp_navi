934a77773cd4eb33df01a4556cd75d475f963544
Akiemon <36184922+sibafb@users.noreply.github.com>
2022-03-27 13:05:17 +0900

    add enum waypoint type

--- src/waypoint.py
+++ src/waypoint.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from enum import ENUM
+
+class WaypointType(ENUM):
+    Coordinate_Point_As_List = auto()
+
+
 class Waypoint():
     """
     Waypont represents a waypoint, whitch is a goal position and orientation of robot. 
@@ -10,6 +16,6 @@ class Waypoint():
         self.x = pos_x
         self.y = pos_y
         self.theta = pos_theta
+        self.Type = WaypointType.Coordinate_Point_As_List
     def toList(self):
         return [self.x, self.y, self.theta]
-        
