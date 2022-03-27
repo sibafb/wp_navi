00dc7f79fcfedaad6360edad3393a25ed1e06994
AkimotoHaruma <siba16fb@gmail.com>
2022-03-27 21:02:23 +0000

    add function

--- src/waypointsList.py
+++ src/waypointsList.py
@@ -5,25 +5,39 @@
 import sys
 sys.path.append(‘../’)
 
+from collections import deque
+
 from waypoint import Waypoint
 
-class WaypointsList():
+class Waypoints():
     """
-    WaypontsList represent a list of waypoints whith are the passing point of waypoint navigation task. 
+    Wayponts represent a list of waypoints whith are the passing point of waypoint navigation task. 
     """
     def __init__(self):
-        self.waypointsList = []
+        self.waypoints = deque()
 
     def append(self,waypoint):
 
         if type(waypoint) is not Waypoint :
             return False
 
-        self.waypointsList.append(waypoint)
+        self.waypoints.append(waypoint)
 
     def pop(self):
 
-        return self.waypointsList.pop()
+        return self.waypoints.pop()
+
+    def dequeue(self):
+
+        return self.waypoints.popleft()
+
+    def dequeue_and_append(self):
+        
+        ret = self.waypoints.popleft()
+        self.waypoints.append(ret)
+
+        return ret
+
 
     
         
