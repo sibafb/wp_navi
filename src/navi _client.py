6dc4ec114f93aaa191769b0248144d31abd74134
AkimotoHaruma <siba16fb@gmail.com>
2022-03-28 21:52:37 +0000

    make some variables private and add call back function

--- src/navi _client.py
+++ src/navi _client.py
@@ -4,7 +4,7 @@
 import rospy
 
 import actionlib
-from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatus, GoalStatusArray
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
@@ -13,16 +13,17 @@ from waypoint import Waypoint
 class NavigationClient():
     def __init__(self, goal_frame = 'map',robot_name = ''): 
 
-        self.goal_frame = goal_frame
+        self.__goal_frame = goal_frame
+        self.__goal_status = GoalStatus.PENDING
 
         ### Action client 
-        self.navClient = actionlib.SimpleActionClient( robot_name + '/move_base', MoveBaseAction)
+        self.__navClient = actionlib.SimpleActionClient( robot_name + '/move_base', MoveBaseAction)
 
-        while not self.ac.wait_for_server(rospy.Duration(30)):
+        while not self.__navClient.wait_for_server(rospy.Duration(30)):
             rospy.loginfo("Waiting for the move_base action server to come up")
 
         ### Subscliber
-        self.navStatus = rospy.Subscliber( robot_name + '/move_base/status', GoalStatusArray,self.cb_status)
+        self.__navStatus = rospy.Subscliber( robot_name + '/move_base/status', GoalStatusArray, self.cb_status)
 
         rospy.loginfo("The servers comes up")
 
@@ -37,14 +38,40 @@ class NavigationClient():
         goal.target_pose.pose.position = wp_pose.position
         goal.target_pose.pose.orientation = wp_pose.orientation
 
-        self.navClient.send_goal(self.goal)
+        self.navClient.send_goal(goal)
     
     def cancel_goal(self):
 
         self.navClient.cancel_goal()
         
     def cb_status(self, status):
-        pass
+
+        if self.__goal_status != status.status_list[-1].status :
+            self.__goal_status = status.status_list[-1].status
+
+            if self.__goal_status == GoalStatus.PENDING:
+                pass
+            elif self.__goal_status == GoalStatus.ACTIVE:
+                pass
+            elif self.__goal_status == GoalStatus.PREEMPTED:
+                pass
+            elif self.__goal_status == GoalStatus.SUCCEEDED:
+                pass
+            elif self.__goal_status == GoalStatus.ABORTED:
+                pass
+            elif self.__goal_status == GoalStatus.REJECTED:
+                pass
+            elif self.__goal_status == GoalStatus.PREEMPTING:
+                pass
+            elif self.__goal_status == GoalStatus.RECALLING:
+                pass
+            elif self.__goal_status == GoalStatus.RECALLED:
+                pass
+            elif self.__goal_status == GoalStatus.LOST:
+                pass  
+
+
+        
 
     def shutdown(self):
         rospy.loginfo("The robot was terminated")
