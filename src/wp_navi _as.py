c398c783ff5146f4c73fe811c37ebe428a8cd113
AkimotoHaruma <siba16fb@gmail.com>
2022-03-29 11:44:01 +0000

    delete unnecessary commetn

--- src/wp_navi _as.py
+++ src/wp_navi _as.py
@@ -14,7 +14,6 @@ from Wpnav.msg import WpnavAction
 from Wpnav.msg import WpnavResult
 from Wpnav.msg import WpnavFeedback
 
-# クラス
 class WpNavi():
     def __init__(self, robot_name = ""): 
         rospy.init_node('wp_navi')
@@ -27,9 +26,9 @@ class WpNavi():
 
         rospy.loginfo("The servers comes up")
 
-        self.goal = MoveBaseGoal()  # ゴールの生成
-        self.goal.target_pose.header.frame_id = 'map'  # 地図座標系
-        self.goal.target_pose.header.stamp = rospy.Time.now()  # 現在時刻
+        self.goal = MoveBaseGoal()
+        self.goal.target_pose.header.frame_id = 'map'
+        self.goal.target_pose.header.stamp = rospy.Time.now() 
 
         # [x,y,theta]
         self.way_point_robot1 = [[ 6.1, 0.8, 0.0 * pi], [6.1, 4.2, 0.5 * pi],
@@ -39,13 +38,10 @@ class WpNavi():
     # シャットダウン時の処理
     def shutdown(self):
         rospy.loginfo("The robot was terminated")
-        self.ac.cancel_goal()  # ゴールをキャンセル
+        self.ac.cancel_goal()
 
 
 if __name__ == '__main__':
-    # 例外処理。rospy.ROSInterruptExceptionを捕まえる。
-    # この例外はCrl+cキーが押されるときに発生するので、
-    # この例外処理によりこのノードが終了する。
     try:
         WpNavi()
         while not rospy.is_shutdown():
