d6a6a1ba635697b186f68f21b18df6dbdf4f6322
AkimotoHaruma <siba16fb@gmail.com>
2022-03-29 11:41:18 +0000

    add robot_name argument

--- src/wp_navi _as.py
+++ src/wp_navi _as.py
@@ -16,11 +16,11 @@ from Wpnav.msg import WpnavFeedback
 
 # クラス
 class WpNavi():
-    def __init__(self), robot_name: 
+    def __init__(self, robot_name = ""): 
         rospy.init_node('wp_navi')
         rospy.on_shutdown(self.shutdown) 
 
-        self.ac = actionlib.SimpleActionClient('robot1/move_base', MoveBaseAction)
+        self.ac = actionlib.SimpleActionClient( robot_name + '/move_base', MoveBaseAction)
 
         while not self.ac.wait_for_server(rospy.Duration(5)):
             rospy.loginfo("Waiting for the move_base action server to come up")
@@ -35,39 +35,7 @@ class WpNavi():
         self.way_point_robot1 = [[ 6.1, 0.8, 0.0 * pi], [6.1, 4.2, 0.5 * pi],
                      [ 2.7, 4.2, -1.0 * pi], [2.7, 0.8, -0.5 * pi],
                      [999, 999, 999]]
-
-        self.i = 0
-
-    def button_pushed(self, button_pushed):
-        if button_pushed.data == True:
-            # ROSではロボットの進行方向がx座標、左方向がy座標、上方向がz座標
-            self.goal.target_pose.pose.position.x = self.way_point_robot1[self.i][0]
-            self.goal.target_pose.pose.position.y = self.way_point_robot1[self.i][1]
-            if self.way_point_robot1[self.i][0] == 999:
-                return
-
-            q = tf.transformations.quaternion_from_euler(0, 0, self.way_point_robot1[self.i][2])
-            self.goal.target_pose.pose.orientation = Quaternion(
-                q[0], q[1], q[2], q[3])
-
-            rospy.loginfo("Sending goal: No" + str(self.i + 1))
-
-            self.ac.send_goal(self.goal)
-            # サーバーにgoalを送信
-
-            # 結果が返ってくるまで30.0[s] 待つ。ここでブロックされる。
-            succeeded = self.ac.wait_for_result(rospy.Duration(30))
-
-            # 結果を見て、成功ならSucceeded、失敗ならFailedと表示
-            state = self.ac.get_state()
-            if succeeded:
-                rospy.loginfo(
-                    "Succeeded: No." + str(self.i + 1) + "(" + str(state) + ")")
-            else:
-                rospy.loginfo(
-                    "Failed: No." + str(self.i + 1) + "(" + str(state) + ")")
-
-            self.i = self.i + 1
+   
     # シャットダウン時の処理
     def shutdown(self):
         rospy.loginfo("The robot was terminated")
