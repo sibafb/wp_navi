1876d2c5ec63f335a6591ca73be5e23477f6e86f
AkimotoHaruma <siba16fb@gmail.com>
2022-03-30 22:07:40 +0000

    add iterata

--- src/waypoints.py
+++ src/waypoints.py
@@ -11,11 +11,12 @@ class Waypoints():
     """
     def __init__(self):
         self.waypoints = deque()
+        self._idx = 0
 
     def append(self,waypoint):
 
-        if type(waypoint) is not Waypoint :
-            return False
+        if type(waypoint) == type(Waypoint) :
+            raise TypeError(str(waypoint) + 'is not waypoint type')
 
         self.waypoints.append(waypoint)
 
@@ -34,7 +35,35 @@ class Waypoints():
 
         return ret
 
+    def __iter__(self):
+        return self
 
-    
-        
-        
+    def next(self):
+        rospy.loginfo("next")
+        if self._idx == len(self.waypoints):
+            raise StopIteration()
+        ret = self.waypoints[self._idx]
+        self._idx += 1
+        return ret
+
+    def __str__(self):
+        return str(self.waypoints)
+
+from math import pi
+import rospy
+
+if __name__ == '__main__':
+
+    rospy.init_node('waypiubts', anonymous=True)
+
+    rospy.loginfo("hello")
+
+    wp = Waypoints()
+    wp.append(Waypoint("first", 6.1, 0.8,  0.0 * pi))
+    wp.append(Waypoint("second", 6.1, 4.2,  0.5 * pi))
+    wp.append(Waypoint("third", 2.7, 4.2, -1.0 * pi))
+    wp.append(Waypoint("fourth", 2.7, 0.8, -0.5 * pi))
+
+    for p in wp:
+        rospy.loginfo("loop")
+        rospy.loginfo(str(p))
