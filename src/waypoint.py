2b663b93e618ed80d8af12b85b073b2d6f4c5d2b
AkimotoHaruma <siba16fb@gmail.com>
2022-03-31 13:08:19 +0000

    make variables private

--- src/waypoint.py
+++ src/waypoint.py
@@ -10,26 +10,29 @@ class Waypoint():
     When the robot is carring on the Navigation Task.  
     """
     def __init__(self, label, pos_x , pos_y, pos_theta):
-        self.x = pos_x
-        self.y = pos_y
-        self.theta = pos_theta
-        self.label = label 
+        self.__x = pos_x
+        self.__y = pos_y
+        self.__theta = pos_theta
+        self.__label = label 
 
     def toList(self):
-        return [self.x, self.y, self.theta]
+        return [self.__x, self.__y, self.__theta]
 
     def toPose(self):
 
         pose = Pose()
 
-        pose.position.x = self.x
-        pose.position.y = self.y
+        pose.position.x = self.__x
+        pose.position.y = self.__y
         pose.position.z = 0
 
-        q = tf.transformations.quaternion_from_euler(0, 0, self.theta)
+        q = tf.transformations.quaternion_from_euler(0, 0, self.__theta)
         pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
 
         return pose
 
+    def labelStr(self):
+        return self.__label
+
     def __str__(self):
-        return "["+str(self.label)+", "+str(self.x)+", "+str(self.y)+", "+str(self.theta)+"]"
+        return "["+str(self.__label)+", "+str(self.__x)+", "+str(self.__y)+", "+str(self.__theta)+"]"
